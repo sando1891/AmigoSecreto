1d8ff843e28937e8591f8b6af51c3eeb33cc0af6
Jose Esteban Sandoval <sando1891@outlook.com>
2025-03-18 01:07:23 -0600

    Se crea función de agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let amigo = inputAmigo.value;
+    if (!amigo) {
+        alert('Por favor inserte un nombre');
+    } else {
+        amigos.push(amigo);
+        inputAmigo.value = '';
+        inputAmigo.focus();
+    }
+}

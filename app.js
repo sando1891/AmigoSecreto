00e0cdb75fe6fa91128134d4ec6f1c9535552017
Jose Esteban Sandoval <sando1891@outlook.com>
2025-03-18 01:58:47 -0600

    Crear la función sortearAmigo

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ function agregarAmigo() {
     let amigo = inputAmigo.value;
     if (!amigo) {
         alert('Por favor inserte un nombre');
+        return;
     } else {
         amigos.push(amigo);
         inputAmigo.value = '';
         inputAmigo.focus();
         mostrarAmigos();
     }
+    return;
 }
+
 function mostrarAmigos() {
     let listaAmigos = document.getElementById('listaAmigos');
     listaAmigos.innerHTML = "";
@@ -20,5 +23,20 @@ function mostrarAmigos() {
         let li = document.createElement('li');
         li.textContent = amigos[i];
         listaAmigos.appendChild(li);
+    }
+    return;
+}
+
+function sortearAmigo() {
+    if(amigos.length === 0){
+        alert('Agrega varios amigos para realizar el sorteo');
+        return;
+    }else{
+        let amigoSorteado = amigos[Math.floor(Math.random()*amigos.length)];
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = `El amigo sorteado es ${amigoSorteado}`
+        let listaAmigos = document.getElementById('listaAmigos');
+        listaAmigos.innerHTML = "";
+        return;
     }
 }
